f7b1c817a04b9438005c24e01d475f43f7c8e992
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2024-07-09 22:15:21 +0300

    Adding a business logic layer; Creating a service for creating and managing records

---
+++ PasswordManager.Application/Interfaces/IRecordSrvice.cs
@@ -0,0 +1,12 @@
+﻿using PasswordManager.Core.Models;
+using PasswordManager.Core.Request;
+
+namespace PasswordManager.Application.Interfaces
+{
+    public interface IRecordSrvice
+    {
+        Task<List<Record>> GetAllRecordsAsync();
+        Task<Record> AddRecordAsync(CreateRecordRequest request);
+        Task<IEnumerable<Record>> SearchRecordsAsync(string searchQuery);
+    }
+}
---
+++ PasswordManager.Application/Services/RecordSrvice.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PasswordManager.Application.Interfaces;
+using PasswordManager.Core.Models;
+using PasswordManager.Core.Request;
+using PasswordManager.DAL;
+
+namespace PasswordManager.Application.Services
+{
+    public class RecordSrvice : IRecordSrvice
+    {
+        private readonly PasswordManagerDbContext _context;
+
+        public RecordSrvice(PasswordManagerDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Record>> GetAllRecordsAsync()
+        {
+            return await _context.Records.OrderByDescending(p => p.DateCreated).ToListAsync();
+        }
+
+        public async Task<Record> AddRecordAsync(CreateRecordRequest request)
+        {
+            if (request.RecordType == Core.Enum.RecordType.Email 
+                && !IsValidEmail(request.Name))
+            {
+                throw new Exception("Неверный формат электронной почты.");
+            }
+
+            var existingRecords = await _context.Records
+                .FirstOrDefaultAsync(r => r.Name == request.Name);
+
+            if (existingRecords != null) 
+            {
+                throw new Exception("Запись с таким названием уже существует.");
+            }
+
+            var record = new Record()
+            {
+                Name = request.Name,
+                Password = request.Password,
+                DateCreated = DateTime.UtcNow,
+                RecordType = request.RecordType
+            };
+
+            _context.Records.Add(record);
+            await _context.SaveChangesAsync();
+
+            return record;
+        }
+
+        public async Task<IEnumerable<Record>> SearchRecordsAsync(string searchQuery)
+        {
+            var searchingResult = await _context.Records
+                .Where(r => r.Name.Contains(searchQuery))
+                .OrderByDescending(r => r.DateCreated)
+                .ToListAsync();
+
+            return searchingResult;
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
