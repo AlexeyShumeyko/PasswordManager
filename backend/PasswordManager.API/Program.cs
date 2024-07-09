4cd94c696b11fcde4abe9d7b78f10bda12ae88b8
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2024-07-09 22:25:45 +0300

    Adding an API Layer

---
+++ backend/PasswordManager.API/Program.cs
@@ -0,0 +1,23 @@
+using PasswordManager.API;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+Startup.ConfigureServices(builder.Services);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
