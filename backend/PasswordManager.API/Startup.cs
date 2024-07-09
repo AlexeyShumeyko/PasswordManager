1538fc11df3505e782a369ca9aa873e1f3664ea4
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2024-07-09 22:27:42 +0300

    Dependency injection control

---
+++ backend/PasswordManager.API/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PasswordManager.Application.Interfaces;
+using PasswordManager.Application.Services;
+using PasswordManager.DAL;
+
+namespace PasswordManager.API
+{
+    public static class Startup
+    {
+        internal static void ConfigureServices(IServiceCollection services)
+        {
+            RegitserDAL(services);
+            RegitserServices(services);
+        }
+
+        private static void RegitserDAL(IServiceCollection services)
+        {
+            services.AddDbContext<PasswordManagerDbContext>(options =>
+                options.UseInMemoryDatabase("PasswordManagerInMemoryDB"));
+        }
+
+        private static void RegitserServices(IServiceCollection services)
+        {
+            services.AddScoped<IRecordSrvice, RecordSrvice>();
+        }
+    }
+}
