860d6a8f31e64ad2c214442bcaef21f0265050d7
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2024-07-09 22:29:31 +0300

    Adding a Record Controller

---
+++ backend/PasswordManager.API/Controllers/RecordController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PasswordManager.Application.Interfaces;
+using PasswordManager.Core.Request;
+
+namespace PasswordManager.API.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class RecordController : ControllerBase
+    {
+        private readonly IRecordSrvice _recordSrvice;
+
+        public RecordController(IRecordSrvice recordSrvice)
+        {
+            _recordSrvice = recordSrvice;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllRecords()
+        {
+            try
+            {
+                var records = await _recordSrvice.GetAllRecordsAsync();
+
+                return Ok(records);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRecord([FromBody] CreateRecordRequest request)
+        {
+            try
+            {
+                var addedRecord = await _recordSrvice.AddRecordAsync(request);
+
+                return Ok(addedRecord);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SearchRecord(string searchQuery)
+        {
+            try
+            {
+                var records = await _recordSrvice.SearchRecordsAsync(searchQuery);
+                return Ok(records);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
