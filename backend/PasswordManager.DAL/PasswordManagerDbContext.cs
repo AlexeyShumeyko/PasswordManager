857f6c11e5edc2949e4eee4be4c236d8bf189671
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2024-07-09 22:08:10 +0300

    Adding an InMemory database; Setting up DbContext

---
+++ backend/PasswordManager.DAL/PasswordManagerDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PasswordManager.Core.Models;
+
+namespace PasswordManager.DAL
+{
+    public class PasswordManagerDbContext : DbContext
+    {
+        public DbSet<Record> Records { get; set; }
+
+        public PasswordManagerDbContext(DbContextOptions<PasswordManagerDbContext> options) 
+            : base(options) 
+        {
+
+        }
+    }
+}
