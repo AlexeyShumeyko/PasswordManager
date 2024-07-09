e8e39d8b90dd7dfc483756d8bd886f66aec128f8
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2024-07-09 22:16:50 +0300

    Adding a request form

---
+++ backend/PasswordManager.Core/Request/CreateRecordRequest.cs
@@ -0,0 +1,18 @@
+﻿using PasswordManager.Core.Enum;
+using System.ComponentModel.DataAnnotations;
+
+namespace PasswordManager.Core.Request
+{
+    public record CreateRecordRequest(
+
+        [Required]
+        [StringLength(100, MinimumLength = 1)]
+        string Name,
+
+        [Required]
+        [MinLength(8)]
+        string Password,
+
+        [Required] 
+        RecordType RecordType);
+}
