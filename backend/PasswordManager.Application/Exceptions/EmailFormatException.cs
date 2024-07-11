68de9286046d5f36e94b1a634384029043f6cb67
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2024-07-11 15:19:11 +0300

    added exception classes for validating incoming requests

---
+++ backend/PasswordManager.Application/Exceptions/EmailFormatException.cs
@@ -0,0 +1,9 @@
+﻿namespace PasswordManager.Application.Exceptions
+{
+    public class EmailFormatException : Exception
+    {
+        public EmailFormatException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ backend/PasswordManager.Application/Exceptions/RecordExistsException.cs
@@ -0,0 +1,9 @@
+﻿namespace PasswordManager.Application.Exceptions
+{
+    public class RecordExistsException : Exception
+    {
+        public RecordExistsException(string message) : base(message)
+        {
+        }
+    }
+}
